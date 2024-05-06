066b78942819b5225c7fea0200d44fde38239001
Tri Nguyen <tri.nguyen.us.java@gmail.com>
2024-05-06 17:50:48 -0500

    first commit

---
+++ src/main/java/com/tri/App.java
@@ -0,0 +1,13 @@
+package com.tri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
